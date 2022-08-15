8ef0622d321063a6f8a51fd5e30d5cf4af276a16
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2022-08-15 13:24:59 +0100

    Create HiFiasm_INPUT1.sh
    Script for running HiFiasm on raw HiFi reads for ST genome assembly

---
+++ Scripts/Genome_Assembly/STgenome/HiFiasm_INPUT1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N ST.HiFiasm1  ##job name
+#$ -l tmem=8G #RAM
+#$ -l h_vmem=8G #enforced limit on shell memory usage
+#$ -l h_rt=20:00:00 ##wall time.  
+#$ -j y  #concatenates error and output files (with prefix job1)
+#$ -pe smp 10
+#$ -R y
+
+
+## Run HiFiasm on the first HiFi raw data generated for the Standard (ST) genome
+## In theory both inputs can be specified in HiFiasm one after the other, but this did not work, so we're running each INPUT separately then merging the results. 
+
+
+#Run on working directory
+cd $SGE_O_WORKDIR 
+
+#Variables
+THREADS=10
+TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+SHAREDPATH=/SAN/ugi/StalkieGenomics
+ST=STgenome
+INPUT1=m64157e_210730_141553.hifi_reads.fastq.gz
+INPUT2=m64157e_211024_013127.hifi_reads.fastq.gz
+
+#Call software
+HIFIASM=/SAN/ugi/StalkieGenomics/software/hifiasm/hifiasm
+
+#Run
+echo "time $HIFIASM -o $SHAREDPATH/$ST/SEPARATE/STgenome_INPUT1_220812.asm --write-paf --write-ec /dev/null \
+--primary $SHAREDPATH/STgenome_rawdata/HiFi/$INPUT1" > hifiasm_$TIMESTAMP.log
+
+time $HIFIASM -o $SHAREDPATH/$ST/SEPARATE/STgenome_INPUT1_220812.asm -t $THREADS --write-paf --write-ec /dev/null \
+--primary $SHAREDPATH/STgenome_rawdata/HiFi/$INPUT1
