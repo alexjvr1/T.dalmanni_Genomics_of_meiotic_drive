b3c7cf486616aaa14a21212bcb1a552415d4f45b
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2022-08-17 12:55:04 +0100

    Update Purge_dups_ST_Round2.sh
    added .fas after $REF.split.fas.
    Check if this works

--- Scripts/Genome_Assembly/STgenome/Purge_dups_ST_Round2.sh
+++ Scripts/Genome_Assembly/STgenome/Purge_dups_ST_Round2.sh
@@ -22,7 +22,8 @@ minimap2=/SAN/ugi/StalkieGenomics/software/minimap2-2.24_x64-linux/minimap2
 pbcstat=/SAN/ugi/StalkieGenomics/software/purge_dups/bin/pbcstat
 calcuts=/SAN/ugi/StalkieGenomics/software/purge_dups/bin/calcuts
 split_fa=/SAN/ugi/StalkieGenomics/software/purge_dups/bin/split_fa
-
+purge_dups=/SAN/ugi/StalkieGenomics/software/purge_dups/bin/purge_dups
+get_seqs=/SAN/ugi/StalkieGenomics/software/purge_dups/bin/get_seqs 
 
 
 #Variables
@@ -46,10 +47,22 @@ done
 # Step 4.3: Split merged assembly by contig and self align
 
 ## Split into contiges
-$split_fa $REF > $REF.split
+$split_fa $REF > $REF.split.fas
 
 ## Self-alignment
-$minimap2 -xasm5 -DP $REF.split $REF.split | gzip -c - > $REF.split.self.paf.gz
+$minimap2 -xasm5 -DP $REF.split.fas $REF.split.fas | gzip -c - > $REF.split.self.paf.gz
 
 
 # Step 4.4: Purge duplicates and extract purged contigs from merged assembly.
+
+#Variables
+
+## Step1: Create a bed file of all duplicate reads
+$purge_dups -2 -T cutoffs -c PB.base.cov $REF.split.self.paf.gz > dups.bed 2> purge_dups.log
+
+
+## Step2: Edit the fas file to remove any colons from the contig names 
+sed 's/:.*//' $REF.split.fas > $REF.split.fas_renamed
+
+## Step3: extract all non-duplicated reads from the original reference
+$get_seqs -e dups.bed $REF.split.fas
