473a1b9320c966a5b1a144cc0d9c9dc573acfee5
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2022-08-17 11:22:16 +0100

    Update minimap2_STgenome_vs_HiFi.sh
    Change alignment to run separately for each set of HiFi raw reads. 
    Add purge_dups steps after alignment

--- Scripts/Genome_Assembly/STgenome/minimap2_STgenome_vs_HiFi.sh
+++ Scripts/Genome_Assembly/STgenome/minimap2_STgenome_vs_HiFi.sh
@@ -6,19 +6,25 @@
 #$ -l h_rt=1:00:00 ##wall time.
 #$ -j y  #concatenates error and output files (with prefix job1)
 
-## Goal: Map raw HiFi reads to draft STgenome assembled with HiFiasm
-
 #Run on working directory
 cd $SGE_O_WORKDIR 
 
 #Software
 minimap2=/SAN/ugi/StalkieGenomics/software/minimap2-2.24_x64-linux/minimap2
+pbcstat=/SAN/ugi/StalkieGenomics/software/purge_dups/bin/pbcstat
+calcuts=/SAN/ugi/StalkieGenomics/software/purge_dups/bin/calcuts
 
 #variables
 REF=/SAN/ugi/StalkieGenomics/STgenome/STgenome_220510.asm.p_ctg.fas
 INPUTREADS=/SAN/ugi/StalkieGenomics/STgenome_rawdata/HiFi
 OUT=/SAN/ugi/StalkieGenomics/STgenome/STgenome_vs_HiFi
 
-time $minimap2 -xasm20 $REF -d $REF.mmi \
-$INPUTREADS/m64157e_210730_141553.hifi_reads.fastq.gz \
-$INPUTREADS/m64157e_211024_013127.hifi_reads.fastq.gz | gzip -c - > $OUT.aln.paf.gz
+#Input files: m64157e_210730_141553.hifi_reads.fastq.gz; m64157e_211024_013127.hifi_reads.fastq.gz
+
+
+for i in $(ls $INPUTREADS/*fastq.gz) 
+do time $minimap2 -xasm20 $REF -d $REF.mmi $i | gzip -c - > $OUT_$i.aln.paf.gz
+done
+
+$pcbstat $OUT/*aln.paf.gz 
+$calcuts $OUT/PB.stat > cutoffs 2> $OUT/calcuts.log
