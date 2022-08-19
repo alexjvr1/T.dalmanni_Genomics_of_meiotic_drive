77a7af641c9b296eda2c1eeb7fe9d4db8de27d4b
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2022-08-19 09:15:12 +0100

    Create kmc_purged_m30.sh

---
+++ Scripts/Genome_Assembly/STgenome/KMC_logfiles/kmc_purged_m30.sh
@@ -0,0 +1,17 @@
+kmc -k21 -ci2 -fa ../purged.fas purged tmp
+*********************
+Stage 1: 100%
+Stage 2: 100%
+1st stage: 8.61198s
+2nd stage: 4.9671s
+Total    : 13.5791s
+Tmp size : 512MB
+
+Stats:
+   No. of k-mers below min. threshold :    344527431
+   No. of k-mers above max. threshold :            0
+   No. of unique k-mers               :    362467637
+   No. of unique counted k-mers       :     17940206
+   Total no. of k-mers                :    425999651
+   Total no. of reads                 :          821
+   Total no. of super-k-mers          :     62089387
