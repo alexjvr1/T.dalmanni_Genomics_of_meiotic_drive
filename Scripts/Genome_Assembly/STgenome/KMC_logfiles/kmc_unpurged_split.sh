0094ccddaed9276e5b5f87a435bb45959656926f
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2022-08-19 09:18:33 +0100

    Create kmc_unpurged_split.sh

---
+++ Scripts/Genome_Assembly/STgenome/KMC_logfiles/kmc_unpurged_split.sh
@@ -0,0 +1,17 @@
+kmc -k21 -ci2 -fa ../../STgenome_220510.asm.p_ctg_split.fas_renamed unpurged_split tmp
+*********************
+Stage 1: 100%
+Stage 2: 100%
+1st stage: 15.8313s
+2nd stage: 5.85943s
+Total    : 21.6907s
+Tmp size : 663MB
+
+Stats:
+   No. of k-mers below min. threshold :    351089936
+   No. of k-mers above max. threshold :            0
+   No. of unique k-mers               :    410708758
+   No. of unique counted k-mers       :     59618822
+   Total no. of k-mers                :    552044407
+   Total no. of reads                 :         1640
+   Total no. of super-k-mers          :     80444408
