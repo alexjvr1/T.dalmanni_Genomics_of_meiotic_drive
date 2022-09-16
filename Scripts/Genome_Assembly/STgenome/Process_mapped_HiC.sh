367c26262b25958616c4d1e7af7a1cb30893f044
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2022-09-16 09:40:13 +0100

    Update Process_mapped_HiC.sh

--- Scripts/Genome_Assembly/STgenome/Process_mapped_HiC.sh
+++ Scripts/Genome_Assembly/STgenome/Process_mapped_HiC.sh
@@ -1,7 +1,41 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N ST.Process_HiC_map  ##job name
+#$ -l tmem=32G #RAM
+#$ -l h_vmem=32G #enforced limit on shell memory usage
+#$ -l h_rt=40:00:00 ##wall time.
+#$ -j y  #concatenates error and output files (with prefix job1)
 
 
+## Goal: Map Hi-C reads to draft ST genome. 
 
 
+#Run on working directory
+cd $SGE_O_WORKDIR 
+
+#Software
+BWA=/share/apps/genomics/bwa-0.7.17/bwa
+export PATH=/share/apps/genomics/samtools-1.14/bin:$PATH
+PICARD=/share/apps/genomics/picard-2.26.9/picard.jar
+
+export PATH=/share/apps/java/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/java/lib:$LD_LIBRARY_PATH
+export PATH=/share/apps/perl-5.30.0/bin/perl5.30.0:$PATH
+
+
+#Variables
+SRA=SRR12620697
+LABEL=STassembly
+FILT_DIR=/SAN/ugi/StalkieGenomics/STgenome/FINAL_PURGEDDUPS/PURGE_l5m23u140/SCAFFOLDING_Hi-C/BAM/FILTERED
+COMBINER=/SAN/ugi/StalkieGenomics/STgenome/FINAL_PURGEDDUPS/PURGE_l5m23u140/SCAFFOLDING_Hi-C/two_read_bam_combiner.pl
+STATS=/SAN/ugi/StalkieGenomics/STgenome/FINAL_PURGEDDUPS/PURGE_l5m23u140/SCAFFOLDING_Hi-C/get_stats.pl
+PAIR_DIR=/SAN/ugi/StalkieGenomics/STgenome/FINAL_PURGEDDUPS/PURGE_l5m23u140/SCAFFOLDING_Hi-C/BAM/PAIRED
+REP_DIR=/SAN/ugi/StalkieGenomics/STgenome/FINAL_PURGEDDUPS/PURGE_l5m23u140/SCAFFOLDING_Hi-C/BAM/DEDUP
+REP_LABEL=$LABEL\_rep1
+MAPQ_FILTER=10
+
+
+**TMP_DIR
 
 echo "Processing of reads mapped using Map_HiC.sh"
 
