6e4b837f388f763bda9154db71d8f6f7ca05aaec
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2022-12-05 11:59:41 +0000

    Update Minimap2_STcollapsed_POM_ST.sh

--- Scripts/Genome_Assembly/Isoseq/Minimap2_STcollapsed_POM_ST.sh
+++ Scripts/Genome_Assembly/Isoseq/Minimap2_STcollapsed_POM_ST.sh
@@ -12,11 +12,19 @@ cd $SGE_O_WORKDIR
 
 #Software
 export PATH=/share/apps/genomics/minimap2/minimap2:$PATH
+export PATH=/share/apps/genomics/samtools-1.16.1/bin:$PATH
 
 #Define variables
 SHAREDPATH=/SAN/ugi/StalkieGenomics
 STREF=$SHAREDPATH/RefGenome/POM_genomes/ST_FINAL.fa
 FASTA=$SHAREDPATH/Blobtools_STgenome/shared_BLAST_fasta_ST
 
-#Mapping
+#Step1: Mapping
 minimap2 -x splice $STREF $FASTA -a -o ST_isoseq_BLASTshared.sam
+
+
+#Step2: sort and index
+
+samtools sort -o ST_isoseq_BLASTshared.sorted.bam ST_isoseq_BLASTshared.sam
+
+samtools index ST_isoseq_BLASTshared.sorted.bam
